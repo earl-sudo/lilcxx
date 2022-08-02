fc022d624f9540664a58f125439c9da24a886ea5
Earl Johnson <earl-johnson78@hotmail.com>
2022-08-02 19:17:40 -0400

    Tring to get test called in github.

--- inc/git_info.h
+++ inc/git_info.h
@@ -2,8 +2,8 @@
 #define GITINFO_H
 
 #define GIT_BRANCH "main"
-#define GIT_HASH "bc4c0c7486cdb07416929e3d7d0dc5a7989ac394"
-#define GIT_DATE "Date:   Sun Jul 31 21:59:55 2022 -0400"
+#define GIT_HASH "0fc2cd4bfa4c1688945a581314c9690b3fb9073d"
+#define GIT_DATE "Date:   Tue Aug 2 19:13:54 2022 -0400"
 
 #endif
 
